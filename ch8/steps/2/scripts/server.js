382413199fb16d1df37f7f64e54612b7bc435df6
Marc Cohen <marccohen@google.com>
2014-09-04 19:25:09 +0100

    server scripts from main directory

--- ch8/steps/2/scripts/server.js
+++ ch8/steps/2/scripts/server.js
@@ -15,7 +15,14 @@ cmd.on('close', function (code) {
       res.sendfile('/src/index.html');
     });
 
-    app.get(/^\/(styles|images|scripts|fonts)\/(.*)/, function (req, res) {
+    app.get(/^\/(scripts)\/(.*)/, function (req, res) {
+      var type = req.params[0];
+      var path = req.params[1];
+      var file = 'scripts/' + type + '/' + path
+      res.sendfile(file);
+    });
+   
+    app.get(/^\/(styles|images|fonts)\/(.*)/, function (req, res) {
       var type = req.params[0];
       var path = req.params[1];
       var file = '/wsk/' + type + '/' + path
