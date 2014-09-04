9822cbfd868a6c6ab88b3d0e4a17a7b029fce626
Marc Cohen <marccohen@google.com>
2014-09-04 09:20:18 +0100

    checkin step 2 files, including those generated by Web Starter Kit

--- ch8/steps/2/scripts/server.js
+++ ch8/steps/2/scripts/server.js
@@ -1,13 +1,28 @@
-var express = require('express');
+var spawn = require('child_process').spawn,
+    cmd = spawn('npm', ['link']);
 
-// Constants
-var PORT = 80;
+cmd.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+  if (code === 0) {
+    var express = require('express');
 
-// App
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World\n');
+    // Constants
+    var PORT = 80;
+
+    // App
+    var app = express();
+    app.get('/', function (req, res) {
+      res.sendfile('/src/index.html');
+    });
+
+    app.get(/^\/(styles|images|scripts|fonts)\/(.*)/, function (req, res) {
+      var type = req.params[0];
+      var path = req.params[1];
+      var file = '/src/' + type + '/' + path
+      res.sendfile(file);
+    });
+
+    app.listen(PORT);
+    console.log('Running on port ' + PORT);
+  }
 });
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
