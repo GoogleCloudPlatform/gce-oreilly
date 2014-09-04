a41286297cf65062d97cf70b68975d077ee3af71
Marc Cohen <marccohen@google.com>
2014-09-04 19:42:36 +0100

    add support for styleguides dir

--- ch8/steps/2/scripts/server.js
+++ ch8/steps/2/scripts/server.js
@@ -22,7 +22,7 @@ cmd.on('close', function (code) {
       res.sendfile(file);
     });
    
-    app.get(/^\/(styles|images|fonts)\/(.*)/, function (req, res) {
+    app.get(/^\/(styles|styleguide|images|fonts)\/(.*)/, function (req, res) {
       var type = req.params[0];
       var path = req.params[1];
       var file = '/wsk/' + type + '/' + path
