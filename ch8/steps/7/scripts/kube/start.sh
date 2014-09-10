49ecadee314223bd4a7ed023f78d0ca469e9ffe7
Marc Cohen <marccohen@google.com>
2014-09-10 15:10:28 +0100

    update with scripts for using kubernetes to manage demo cluster

---
+++ ch8/steps/7/scripts/kube/start.sh
@@ -0,0 +1,4 @@
+kubecfg.sh -c service-pubsub.json create services
+kubecfg.sh -c service-p2p.json create services
+kubecfg.sh -c pod-perfuse-master.json create pods
+kubecfg.sh -c controller-perfuse-slave.json create replicationControllers
