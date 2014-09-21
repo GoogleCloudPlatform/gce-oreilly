93191869d021ed126af323f8222750beea219d7a
Marc Cohen <marccohen@google.com>
2014-09-21 11:01:37 +0100

    moving steps into parent directory

--- ch8/steps/7/scripts/kube/stop.sh
+++
@@ -1,4 +0,0 @@
-kubecfg.sh delete services/service-pubsub
-kubecfg.sh delete services/service-p2p
-kubecfg.sh stop pods/pod-perfuse-master.json
-kubecfg.sh stop replicationControllers/controller-perfuse-slave.json
